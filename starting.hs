ff2c3ef94ffbbe8eeccb65ad42db73fded4fa3da
Rafal Szymanski <me@rafal.io>
2015-10-25 01:23:06 -0400

    brackets

--- starting.hs
+++ starting.hs
@@ -5,41 +5,42 @@ import Text.Parsec
 
 
 {-
-1 + 2 - 4
-
-need to left-factor
-
-e := int
-e := e '+' e
-e := e '-' 'e'
-
-
 e := term '+' e
 e := term '-' e
 e := t
 
 term := int
+term := var
+term := '(' e ')'
 
 -}
 
-data NumExpr =   ExprInt Int
---               | ExprAdd NumExpr NumExpr
- --              | ExprSub NumExpr NumExpr
-               | BinExpr NumExpr BinOp NumExpr
+data Expr      = ExprInt Int
+               | BinExpr Expr BinOp Expr
+               | Var String
                deriving (Eq, Show)
 
 data BinOp = Add | Sub deriving (Eq, Show)
 
 type SimpleParser a = Parsec String () a
 
-parseExprInt :: SimpleParser NumExpr
-parseExprInt = many1 digit >>= \n -> (return $ ExprInt (read n))
-
 lexeme :: Parsec String u a -> Parsec String u a
 lexeme p = p >>= \x -> (skipMany space) >> return x
 
-term :: SimpleParser NumExpr
-term = lexeme parseExprInt
+parseExprInt :: SimpleParser Expr
+parseExprInt = many1 digit >>= \n -> (return $ ExprInt (read n))
+
+parseExprVar :: SimpleParser Expr
+parseExprVar = many1 letter >>= \x -> return $ Var x
+
+term :: SimpleParser Expr
+term = lexeme (parseExprInt <|> parseExprVar <|> (bracketParser parseNumExpr))
+
+bracketParser p = do
+  lexeme $ char '('
+  x <- lexeme p
+  lexeme $ char ')'
+  return x
 
 parseBinOp :: SimpleParser BinOp
 parseBinOp = lexeme (char '+' >> return Add) <|> (char '-' >> return Sub)
@@ -50,15 +51,11 @@ parseNumExpr = do
   rest <- many (parseBinOp >>= \op -> term >>= \t -> return (op,t))
   return $ foldl (\x (f, y) -> BinExpr x f y) t rest
 
---sampleExpr = ExprSub (ExprInt 12) (ExprAdd (ExprInt 2) (ExprInt 3))
-
-evalExpr :: NumExpr -> Int
+evalExpr :: Expr -> Int
 evalExpr (ExprInt i)    = i
 evalExpr (BinExpr l Add r)  = (evalExpr l) + (evalExpr r)
 evalExpr (BinExpr l Sub r)  = (evalExpr l) - (evalExpr r)
 
 main :: IO ()
 main = undefined
-
-
 
