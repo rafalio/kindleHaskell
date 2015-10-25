944ca3302dede176d3c4dd2c37b02fb16b3486e9
Rafal Szymanski <me@rafal.io>
2015-10-25 01:06:16 -0400

    left factor

--- starting.hs
+++ starting.hs
@@ -3,43 +3,59 @@
 import Control.Monad
 import Text.Parsec
 
+
+{-
+1 + 2 - 4
+
+need to left-factor
+
+e := int
+e := e '+' e
+e := e '-' 'e'
+
+
+e := term '+' e
+e := term '-' e
+e := t
+
+term := int
+
+-}
+
 data NumExpr =   ExprInt Int
-               | ExprAdd NumExpr NumExpr
-               | ExprSub NumExpr NumExpr
+--               | ExprAdd NumExpr NumExpr
+ --              | ExprSub NumExpr NumExpr
+               | BinExpr NumExpr BinOp NumExpr
                deriving (Eq, Show)
 
-
-
+data BinOp = Add | Sub deriving (Eq, Show)
 
 type SimpleParser a = Parsec String () a
-
 
 parseExprInt :: SimpleParser NumExpr
 parseExprInt = many1 digit >>= \n -> (return $ ExprInt (read n))
 
-parseExprAdd = do
-  l <- parseNumExpr
-  char '+'
-  r <- parseNumExpr
-  return $ ExprAdd l r
+lexeme :: Parsec String u a -> Parsec String u a
+lexeme p = p >>= \x -> (skipMany space) >> return x
 
-parseExprSub = do
-  l <- parseNumExpr
-  char '-'
-  r <- parseNumExpr
-  return $ ExprSub l r
+term :: SimpleParser NumExpr
+term = lexeme parseExprInt
 
-parseNumExpr :: SimpleParser NumExpr
+parseBinOp :: SimpleParser BinOp
+parseBinOp = lexeme (char '+' >> return Add) <|> (char '-' >> return Sub)
+
+-- Instead of left-recursion, use iteration
 parseNumExpr = do
-  parseExprInt <|> parseExprAdd <|> parseExprSub
+  t <- term
+  rest <- many (parseBinOp >>= \op -> term >>= \t -> return (op,t))
+  return $ foldl (\x (f, y) -> BinExpr x f y) t rest
 
-sampleExpr = ExprSub (ExprInt 12) (ExprAdd (ExprInt 2) (ExprInt 3))
+--sampleExpr = ExprSub (ExprInt 12) (ExprAdd (ExprInt 2) (ExprInt 3))
 
 evalExpr :: NumExpr -> Int
 evalExpr (ExprInt i)    = i
-evalExpr (ExprAdd l r)  = (evalExpr l) + (evalExpr r)
-evalExpr (ExprSub l r)  = (evalExpr l) - (evalExpr r)
---evalExpr (ExprMul l r)  = (evalExpr l) * (evalExpr r)
+evalExpr (BinExpr l Add r)  = (evalExpr l) + (evalExpr r)
+evalExpr (BinExpr l Sub r)  = (evalExpr l) - (evalExpr r)
 
 main :: IO ()
 main = undefined
